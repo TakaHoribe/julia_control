563ac23f17154d4e9a39cb85c8c2adf021473fb8
Takamasa Horibe <horibe.takamasa@gmail.com>
2018-09-23 01:09:36 +0900

    test.ji: add findNearestPose finction

--- test.jl
+++ test.jl
@@ -10,7 +10,7 @@ using LinearAlgebra
 # set_default_plot_size(9inch, 9inch/golden);
 t = 0:0.1:10
 x = sin.(t)
-y = rand(10)
+y = rand(101)
 plot(x, y, linewidth=2, title="My Plot")
 scatter(x, y)
 
@@ -44,3 +44,53 @@ I = X * pinv(X)
 
 println(C)
 println(D)
+
+
+# ===== find nearest point =====
+function findNearestPoint(path_x, path_y, self_x, self_y)
+    dist_squared = (path_x .- self_x).^2 + (path_y .- self_y).^2
+    (min_dist_squared, min_index) = findmin(dist_squared)
+    return (sqrt(min_dist_squared), min_index)
+end
+
+
+# ===== trajectory design =====
+x = 0:1:10
+y = sin.(x)
+scatter(x, y)
+# dy/dx = cos(x)からthetaを求める
+dydx = cos.(x) # = tan(theta)
+theta = atan.(dydx)
+scatter(x, theta)
+
+
+# ===== robot model =====
+#
+# -- continuous --
+# dot x     = v * cos (theta)
+# dot y     = v * sin (theta)
+# dot theta = w
+#
+# X = [x, y, theta]'
+# U = [v, w]'.
+#
+#  -- discrete --
+# x_k+1     = x_k + v_k * cos(theta_k) * dt
+# y_k+1     = y_k + v_k * sin(theta_k) * dt
+# theta_k+1 = theta_k + w_k * dt
+#
+# dF(X,U)/dX = [1, 0, -v*sin(theta)*dt;
+#               0, 1, v*cos(theta)*dt;
+#               0, 0, 1]
+#
+# dF(X,U)/dU = [cos(theta)*dt, 0;
+#               sin(theta)*dt, 0;
+#               0,            dt]
+
+
+
+# ==== test ====
+self_x = 3
+self_y = 3
+
+(min_dist, i) = findNearestPoint(x, y, self_x, self_y)
